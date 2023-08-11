bf69fe832a8af390adb43b0744c7438d8e300829
sousoutomato <nadamaach17@gmail.com>
2023-08-11 14:41:16 -0700

    klm

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main -entry point 
+i* description
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+		return (0);
+}
