91a62bb57f2bfd2b81a2ea03f342d0edda98a8fc
sousoutomato <nadamaach17@gmail.com>
2023-08-11 10:45:50 -0700

    ghj

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* description for main if the number is postive or negative */
 int main(void)
 {
 	int n;
