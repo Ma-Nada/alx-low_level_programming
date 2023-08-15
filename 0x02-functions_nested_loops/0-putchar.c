85bbc4df0b1c84caefeb0794d1b248cc83252285
sousoutomato <nadamaach17@gmail.com>
2023-08-15 15:18:19 -0700

    task of functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(void)
+{
+	return (write(1, "_putchar\n", 8));
+}
+int main(void)
+{
+int _putchar(void);
+	return (0);
+}
