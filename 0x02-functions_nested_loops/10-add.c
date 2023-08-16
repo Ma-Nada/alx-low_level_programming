e45ab36bcb115a77ac7b6113790a4fb940824364
sousoutomato <nadamaach17@gmail.com>
2023-08-15 19:49:08 -0700

    task 10 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Description: 'the program's description'
+ * @c: describe the parameter
+ * @d: description
+ * Return: Always 0 (Success)
+ */
+int add(int, int)
+{
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ *
+ * Description: 'the program's description'
+ * @n: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+}
