96d975d7f99a625e47ac3ad076483a0e18efd4e7
sousoutomato <nadamaach17@gmail.com>
2023-08-15 17:52:29 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+			return (1);
+	}
+			return (0);
+}
+
