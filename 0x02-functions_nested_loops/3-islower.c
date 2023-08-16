7d68ec4aa90381b2eb140dc34b698852113b0bcd
sousoutomato <nadamaach17@gmail.com>
2023-08-15 17:42:56 -0700

    task 3 with problem c descripton

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 /**
  * _islower - Entry point
  *  Return: 0 or 1
- *  'c' - Entry point
+ *  @c: the character to be checked
  *  description: c is a variable
  */
 int _islower(int c)
 {
-	if (c >= 97 || c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }
