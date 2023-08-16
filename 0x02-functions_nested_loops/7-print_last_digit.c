7eff8eaadff9203f3c076f65d86c70d5e2f52da4
sousoutomato <nadamaach17@gmail.com>
2023-08-15 18:43:17 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * Description: 'the program's description'
+ * @c: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int c)
+{
+	int ch;
+
+	ch = c % 10;
+	return (ch);
+}
