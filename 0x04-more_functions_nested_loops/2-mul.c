8ab87dd8cc3ed37facf43b85189eeb95c752d5c1
sousoutomato <nadamaach17@gmail.com>
2023-08-17 06:56:07 -0700

    task 1 and 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ *
+ * Description: 'the program's description'
+ * @a: describe the parameter
+ * @b: integer
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
