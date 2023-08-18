548ca241c4918aa0e55c68428abf974b8eef3fb8
sousoutomato <nadamaach17@gmail.com>
2023-08-18 16:01:10 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,15 +12,17 @@ void print_square(int size)
 	int i;
 	int j;
 
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (size <= 0)
-				_putchar('\n');
-			else
 			_putchar('#');
 		}
 		_putchar('\n');
+	}
 	}
 }
