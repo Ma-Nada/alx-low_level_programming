ba5bba6ce0e28493ed4959a8a6d4152c871fee9e
sousoutomato <nadamaach17@gmail.com>
2023-08-21 15:54:08 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ *
+ * Description: 'the program's description'
+ * @s: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	char *pt;
+	int n = 0;
+	char *k = s;
+
+	while (*k++)
+		n++;
+	for (pt = s + n; pt > s; pt--)
+	{
+		_putchar(*pt);
+	}
+	_putchar('\n');
+}
