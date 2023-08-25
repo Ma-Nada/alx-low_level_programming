7282c715da51863195d3cc0e173986dae6dbef44
sousoutomato <nadamaach17@gmail.com>
2023-08-25 15:19:39 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 != '\0') && (*s2 != '\0'))
+	while ((*s1 == *s2) && (*s2 == *s1))
 	{
-		if (*s1 > *s2)
-			return (15);
-		else if (*s1 < *s2)
-			return (-15);
 		s1++;
 		s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
