332f6e42a999ee293de2696820fcbd53b2120c52
sousoutomato <nadamaach17@gmail.com>
2023-08-25 15:40:14 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - Entry point
+ *
+ * Description: 'the program's description'
+ * @n: describe the parameter
+ * @a: description
+ * Return: Always 0 (Success)
+ */
+void reverse_array(int *a, int n)
+{
+	/*while (n--)
+	{
+		printf("%d", a[n - 1]);
+	}*/
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
