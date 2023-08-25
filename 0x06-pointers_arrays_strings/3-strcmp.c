c5cd8134f66dc9e3da4652f222899b6a47c1a928
sousoutomato <nadamaach17@gmail.com>
2023-08-25 09:31:27 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - Entry point
+ *
+ * Description: 'the program's description'
+ * @s1: describe the parameter
+ * @s2: description
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0') && (*s2 != '\0'))
+	{
+		if (*s1 > *s2)
+			return 1;
+		else if (*s1 < *s2)
+			return -1;
+		s1++;
+		s2++;
+	}
+	return 0;
+}
