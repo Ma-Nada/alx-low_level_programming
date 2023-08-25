aab14ee2ca840b2fa71310edeab704714de214e1
sousoutomato <nadamaach17@gmail.com>
2023-08-25 16:05:59 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,21 +10,8 @@
  */
 void reverse_array(int *a, int n)
 {
-	/**
-	 * int i;
-*
-*	i = n;
-*	while (i >= 0)
-*	{
-*		i--;
-*		printf("%d", a[i]);
-*		if (i == 0)
-*			break;
-*		printf(", ");
-*	}
-*	printf("\n");
-*	*/
 	int tmp, index;
+
 	for (index = n - 1; index >= n / 2; index--)
 	{
 		tmp = a[n - 1 - index];
