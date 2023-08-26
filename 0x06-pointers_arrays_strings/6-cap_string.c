11a66dea555c1ab080355b021cbb946a87bd0233
sousoutomato <nadamaach17@gmail.com>
2023-08-25 17:33:43 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - Entry point
+ *
+ * Description: 'the program's description'
+ * @str: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
