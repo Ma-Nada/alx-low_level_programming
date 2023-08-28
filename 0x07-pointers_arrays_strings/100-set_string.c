55d01caf0f19bfe3efd44e29c3c064e45f6184c3
sousoutomato <nadamaach17@gmail.com>
2023-08-28 10:58:49 -0700

    task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - Entry point
+ *
+ * Description: 'the program's description'
+ * @s: describe the parameter
+ * @to: description
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
