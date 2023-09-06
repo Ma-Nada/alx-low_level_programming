e9b28a3928e47a008055760b64d54385ba2026f0
sousoutomato <nadamaach17@gmail.com>
2023-09-05 19:03:04 -0700

    task 5

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - Entry point
+ *
+ * Description: 'the program's description'
+ * @grid: describe the parameter
+ * @height; description
+ * Return: Always 0 (Success)
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+				}
+				free(grid);
+				}
