6a68df462856cdb159eb3fadd513091e50e671ce
sousoutomato <nadamaach17@gmail.com>
2023-09-05 18:12:52 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * str_concat - Entry point
+ *
+ * Description: 'the program's description'
+ * @s1: describe the parameter
+ * @s2: the string that should be added to the first one
+ * Return: Always 0 (Success)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int i = 0, j = 0, k = 0, l = 0, add;
+
+	while (s1[l])
+		l++;
+	while (s2[k])
+		k++;
+	add = l + k;
+	if (s2 == NULL)
+		return (NULL);
+	concat = malloc(add * sizeof(char));
+	if (concat == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		concat[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		concat[i] = s2[j];
+		i++;
+		j++;
+	}
+	concat[i] = '\0';
+	return (concat);
+}
