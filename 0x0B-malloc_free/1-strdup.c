f5e7036ca1ac3f3b1ae2f6561a7c5719c86d0e31
sousoutomato <nadamaach17@gmail.com>
2023-09-05 17:49:45 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * _strdup - Entry point
+ *
+ * Description: 'the program's description'
+ * @str: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i = 1, a = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while(str[i])
+		i++;
+	p = malloc(i * sizeof(char) + 1);
+	if (p == NULL)
+		return (NULL);
+	while (a < i)
+	{
+		p[a] = str[a];
+		a++;
+	}
+	p[a] = '\0';
+	return (p);
+}
