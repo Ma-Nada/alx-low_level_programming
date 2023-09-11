83c51b32b58703969286a3aebb7a12b758ff6a54
sousoutomato <nadamaach17@gmail.com>
2023-09-11 15:02:04 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - Entry point
+ *
+ * Description: 'the program's description'
+ * @name: description
+ * @age: description
+ * @owner: description
+ * Return: Always 0 (Success)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
