e08ba4fece01bb16c9d7e743ef34eb182129ea20
Developer-Nada <nadamaach17@gmail.com>
2024-01-11 01:46:53 +0100

    task0 recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-/*_puts_recursion - print a string
+/**
+ * _puts_recursion - print a string
  * s: the string to print
  * return: nothing
  */
 void _puts_recursion(char *s)
 {
-		printf("%s",s);
+		printf("%s", s);
 	printf("\n");
 }
