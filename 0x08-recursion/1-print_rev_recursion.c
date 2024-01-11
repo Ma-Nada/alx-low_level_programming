460dcb43a5df2d46aa6bf3b180a602efffa9ce37
Developer-Nada <nadamaach17@gmail.com>
2024-01-11 02:45:11 +0100

    task 1 recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - inverse a string
+ * @s: the string to be reversed
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	while (i != 0)
+	{
+		i--;
+	       printf("%c", s[i]);
+	}
+	printf("\n");
+}	
