6c0b494b38697608290588c5c7ccbd10a8207cbf
Developer-Nada <nadamaach17@gmail.com>
2024-01-12 03:32:54 +0100

    task 5 recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 #include "main.h"
 	/**
 	 * _sqrt_recursion - Returns the natural square root of a number
-	 * description: returns the root of a number
+	 * Description: returns the root of a number
 	 *
 	 * @m : number to calculate the root for
 	 * Return: the natural square root
@@ -13,7 +13,7 @@
 	}
 	/**
 	 * _sqrt - calculates natural square root
-	 *
+	 * Description: the root
 	 * @n: number to calculate the square root
 	 * Return: the natural square root
 	 */
