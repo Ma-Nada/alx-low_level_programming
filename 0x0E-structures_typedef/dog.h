9a938001dd7e2fff07e9edbb5879bbc19ff9f112
Developer-Nada <nadamaach17@gmail.com>
2024-01-22 15:37:20 -0800

    corrections

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 /*
- * struct dog - a dog's basic info
+ * dog - a dog's basic info
+ * Description: the program's description
  * @name: First member
  * @age: Second member
  * @owner: Third member
- * Description: Longer description
  */
 struct dog
 {
