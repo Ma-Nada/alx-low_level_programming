cbb7162973f4fb09618134338e6816070254bdcc
Developer-Nada <nadamaach17@gmail.com>
2024-02-08 12:29:49 -0800

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+#include <stddef.h>
 /**
  * binary_to_uint - converts binary to unsigned int
  * @b: the variable
@@ -6,4 +7,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int s, i;
 
+	s = 0;
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		s <<= 1;
+		if (b[i] == '1')
+			s += 1;
+	}
+	return (s);
+}
