efc1842aa52a3dbbdeb1d72f9c8cf2a405ed8b97
Developer-Nada <nadamaach17@gmail.com>
2024-03-01 17:11:51 -0800

    tasks

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <ctype.h>
 #include "main.h"
+/**
+ * _strcpy - copy
+ * @dest: dest
+ * @src: src
+ * Return: char
+ */
 char *_strcpy(char *dest, char *src)
 {
 	return (strcpy(dest, src));
