27fa9393bf891854547cea4c79068b5db5404888
Developer-Nada <nadamaach17@gmail.com>
2024-03-01 17:06:20 -0800

    tasks

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -3,7 +3,30 @@
 #include <string.h>
 #include <ctype.h>
 #include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	return (haystack, needle);
+	char *l = haystack;
+	char *p = needle;
+
+	for (; *haystack != '\0'; haystack++)
+	{
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
 }
+
